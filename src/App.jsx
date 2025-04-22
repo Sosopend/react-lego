cc3051d7516f1d72cc4cbed71c76e7c54b6e68b5
sofiane <pendelliausofiane@gmail.com>
2025-04-22 15:07:15 +0200

    Add Header &  Footer files in components dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Header from './components/Header/Header';
 import Titanic from './components/Titanic/Titanic';
 import MilleniumFalcon from './components/MilleniumFalcon/MilleniumFalcon';
+import Footer from './components/Footer/Footer';
 import "./App.css";
 
 
 function App() {
   return (
     <>
+      <Header />
       <Titanic />
       <MilleniumFalcon />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <p>© 2025 Tous droits réservés.</p>
+    </footer>
+  );
+}
+
+export default Footer;
