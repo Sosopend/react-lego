6f434002b2d23b24ec959809f697cdfad6bbe3d4
sofiane <pendelliausofiane@gmail.com>
2025-04-22 11:59:15 +0200

    first push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function Card() {
+  return (
+    <div>
+      <h1>ma carte</h1>
+      <p>première carte react</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+      <Card/>
+  );
+}
+
+export default App;
