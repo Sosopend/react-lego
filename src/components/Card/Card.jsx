7eb285240822d240eaf9728199a1ad2468465801
sofiane <pendelliausofiane@gmail.com>
2025-04-23 16:15:25 +0200

    affichage conditionnel

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,13 @@
 import "./card.css";
 
-function Card({ name, tag, price, imageSrc }) {
+function Card({ name, tag, price, imageSrc, available }) {
   return (
     <div className="product-card">
       <img className="images" src={imageSrc} alt={name} />
       <h2>{name}</h2>
       <p className="tag">{tag}</p>
       <p>Prix : {price} €</p>
+      <p>{available ? "Disponible" : "En rupture"}</p>
       <button>Ajouter</button>
     </div>
   );
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,27 +1,24 @@
 import Card from "../Card/Card";
+import products from "../../../data/product"
 import "./list.css";
 
 function List() {
   return (
     <div className="product-list">
-      <Card
-        name="Titanic"
-        tag="Historique"
-        price={199.99}
-        imageSrc="https://www.lego.com/cdn/cs/set/assets/blt6cdf0b53146b5519/10294_Prod.png?format=webply&fit=bounds&quality=100&width=400&height=400&dpr=1"
-      />
-      <Card
-        name="Millennium Falcon"
-        tag="cinema"
-        price={299.99}
-        imageSrc="https://www.lego.com/cdn/cs/set/assets/blt3349f56c6f192e18/75192_Prod.png?format=webply&fit=bounds&quality=100&width=400&height=400&dpr=1"
-      />
-      <Card
-        name="Nuit étoilée - Van Gogh"
-        tag="arts"
-        price={169.99}
-        imageSrc="https://www.lego.com/cdn/cs/set/assets/bltc6d87e5e7bacb3ae/21333.png?format=webply&fit=bounds&quality=100&width=400&height=400&dpr=1"
-      />
+      {products.map(
+        (product, index) => (
+          product.available && (
+          <Card
+            key={index}
+            name={product.name}
+            tag={product.tag}
+            price={product.price}
+            available={product.available}
+            imageSrc={product.imageSrc}
+          />
+        )
+        )
+      )}
     </div>
   );
 }
